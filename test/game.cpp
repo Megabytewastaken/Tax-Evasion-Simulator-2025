9516deef6169cce1df737f68d745bcc70b706f56
Pablo Marin <Pablomarin628@gmail.com>
2025-07-11 10:44:38 -0700

    sigma

--- test/game.cpp
+++ test/game.cpp
@@ -95,7 +95,7 @@ int main()
 			SetWindowTitle("Tax Evasion Simulator 2025 (NO ADS)");
 			HideCursor();
 			SetClipboardText("I HATE THE IRS!");
-			OpenURL("https://www.youtube.com/watch?v=mt6O3US9IE4");
+			OpenURL("https://www.youtube.com/watch_popup?v=mt6O3US9IE4");
 		}
 		if (mainmenu)
 		{

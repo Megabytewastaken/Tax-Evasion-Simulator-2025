01baaed4f408b74b7e46cd6b3c17b93fdc48a3e7
franklinW2010 <franklinwashington5030@gmail.com>
2025-07-11 07:34:37 -0700

    working on wasd file j*b

--- test/character.cpp
+++ test/character.cpp
@@ -1,1 +1,31 @@
 #include "character.h"
+
+
+
+void character::getMovementInput(//"direction")
+{
+		if (IsKeyDown(KEY_A) && circenx > 50)
+		{
+			circenx = circenx - speed;
+		}
+		if (IsKeyDown(KEY_S) && circeny < winhei - 50)
+		{
+			circeny = circeny + speed;
+		}
+		if (IsKeyDown(KEY_D) && circenx < winwid - 50)
+		{
+			circenx = circenx + speed;
+		}
+		if (IsKeyDown(KEY_W) && circeny > 50)
+		{
+			circeny = circeny - speed;
+		}
+		if (IsKeyDown(KEY_LEFT_SHIFT))
+		{
+			speed = basespeed * 2;
+		}
+		else
+		{
+			speed = basespeed;
+		}
+}
